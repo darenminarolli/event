20247cb12b0e25a0b603795f2cf323dc6a9189f8
darenminarolli <Daren>
2024-09-15 23:46:05 +0200

    now

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -8,12 +8,11 @@ interface PropsType {
 }
 
 const UserCard: React.FC<PropsType> = ({ user, handleDelete, openEditModal }) => {
-  if (user && user.firstName)
   return (
     <div className="w-full flex justify-between items-center p-6 gap-y-4 border-effect bg-slate-200 text-darkText">
       <div className="w-full"> 
         <h2 className="text-2xl font-bold secondary-text">{user.role ==='admin' ? 'Admin' : 'User'} Card</h2>
-        <p className="text-lg font-bold">Name: {user.firstName} {user.lastName}</p>
+        <p className="text-lg font-bold">Name: {user.lastName}</p>
         <p className="text-lg font-bold">Email: {user.email}</p>
       </div>
       <div className="w-full space-y-4">
